0110dd5a6721a3eed928a370fa65d99e17cb314e
vasil94 <basilsocl@gmail.com>
2021-09-04 21:19:32 +0300

    Add account activations

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  test "account_activation" do
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
+    assert_equal "Account activation", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,              mail.text_part.body.encoded
+    assert_match user.name,               mail.html_part.body.encoded
+    
+    assert_match user.activation_token,   mail.text_part.body.encoded
+    assert_match user.activation_token,   mail.html_part.body.encoded
+    assert_match CGI.escape(user.email),  mail.text_part.body.encoded
+    assert_match CGI.escape(user.email),  mail.html_part.body.encoded
+  end
+end
